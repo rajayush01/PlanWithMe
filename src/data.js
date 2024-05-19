f5cfad3963d3c61307bffd099a6d1431d60399a7
Ayush Raj <119657235+rajayush01@users.noreply.github.com>
2024-05-19 14:40:08 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = [
+	{
+		id: 1,
+		name: "Dehradun",
+		info: "Dehradun is a sprawling city with its speckled picturesque places, vedent landscape, temples, picnic spots which enhance its natural beauty. This pictorial place is an ideal hotspot for those who really want to take risks in their life. A wide range of escapades is in offer in this valley. With the enormous places for Paragliding, Skiing, Angling and River Rafting.",
+		image: "https://www.namastedehradun.com/wp-content/uploads/2013/02/musoorie1.jpg",
+		price: "55,758",
+	},
+	{
+		id: 2,
+		name: "Leh",
+		info: `Located at a height of 11,500 feet, Leh finds place in the bucket list of many travel enthusiasts. It will leave you in awe with its iconic monasteries and picturesque landscapes.Prepare to be wooed by the mesmerizing mountains of Leh. You will love sightseeing and the surreal vistas of snow-capped mountains. Leh is also known for its unique wildlife.`,
+		image: "https://hblimg.mmtcdn.com/content/hubble/img/leh/mmt/destination/m_leh-landscape_l_400_640.jpg",
+		price: "82,560",
+	},
+	{
+		id: 3,
+		name: "Amritsar",
+		info: "Soak in the mystic hues of Amritsar, a famous pilgrimage spot! You will love exploring sightseeing attractions and attending various festivals. The local cuisines at Amritsar are worth trying out too.Home to the legendary Sikh shrine Golden Temple, Amritsar is fondly called Ambarsar and will steal your heart with its colourful bazaars, lip-smacking street food, shopping hubs and historical monuments.",
+		image: "https://hblimg.mmtcdn.com/content/hubble/img/amritsar/mmt/destination/m_destination_Amritsar_l_367_623.jpg",
+		price: "68,595",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
+		image: "https://cdn.britannica.com/13/189813-050-BF6163E7/Manikarnika-Ghat-cremation-site-Hindu-Ganges-River.jpg",
+		price: "31,095",
+	},
+	{
+		id: 5,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQ7v9xgHMb0Hl3vuvnXpPj65kJqFH0R3_RackUj6Y3g-A3U9aVbmxmvLM-8HGlUBgj0l-0DfL97HKfE85GJcS_2FGt7HXDtUVxsX4fl3Q",
+		price: "78,595",
+	},
+	{
+		id: 6,
+		name: "Srinagar",
+		info: "Srinigar is a modern waterworld, dominated by Dal Lake and its twisting waterways, tree-lined Nagin Lake, and the Jhelum River. Engulf yourself in local culture by embracing your sea legs and renting one of the wooden boats called shikaras for a daytime or twilight cruise. On land, stroll through the terraced hillsides of the 400-year-old Mughal Gardens, created by Emperor Jehangir for his wife, and shop for indigenous crafts like hand-woven silks and embroidered shawls.",
+		image: "https://hblimg.mmtcdn.com/content/hubble/img/srinagar/mmt/destination/m_Srinagar_activity_mountains_l_477_716.jpg",
+		price: "68,595",
+	},
+	{
+		id: 7,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: "https://hblimg.mmtcdn.com/content/hubble/img/goa/mmt/destination/m_destination-goa-landscape_l_400_640.jpg",
+		price: "29,695",
+	},
+];
+
+export default data;
